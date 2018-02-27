c1dd3fe5f880bf80e9dbb24668f1ca22a8299cde
augustokal <augustokalel2@yahoo.com.br>
2018-02-27 14:45:38 -0300

    Versão inicial
    Programa simples orientado a objetos, apenas para capturar idade altura e nome no c#

--- Pessoa/Program.cs
+++ Pessoa/Program.cs
@@ -9,7 +9,30 @@ namespace Pessoa
     class Program
     {
         static void Main(string[] args)
+            //Faça um projeto que armazene idade altura e peso
         {
+
+            pessoa pessoa1 = new pessoa();
+
+            Console.WriteLine("Insira seus dados");
+
+            Console.WriteLine("Informe seus dados");
+
+
+
+            Console.WriteLine("Informe seu nome");
+            pessoa1.nome = Console.ReadLine();
+
+            Console.WriteLine("Informe sua idade");
+            pessoa1.idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe sua altura");
+            pessoa1.altura = Convert.ToDouble(Console.ReadLine());
+
+            pessoa1.sobreVoce();
+            Console.ReadKey();
+
+
         }
     }
 }
---
+++ Pessoa/pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pessoa
+{
+    class pessoa
+    {
+        public string nome;
+        public double altura;
+        public int idade;
+
+        public void sobreVoce()
+        {
+
+
+            Console.WriteLine("My name is: " + nome);
+            Console.WriteLine("Eu tenho  " +altura+ "de altura.");
+            Console.WriteLine("Minha idade é: " + idade);
+
+
+
+        }
+
+
+    }
+}
